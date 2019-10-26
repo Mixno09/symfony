d2ab3a134c34ee0babe41850f07c08f8d163b33d
Mixno <mixno09@mail.ru>
2019-10-26 16:27:56 +0300

    Created entity Product

---
+++ src/Entity/Product.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+class Product
+{
+    /**
+     * @var int
+     */
+    public $id = 0;
+    /**
+     * @var string
+     */
+    public $title = '';
+    /**
+     * @var string
+     */
+    public $description = '';
+    /**
+     * @var string
+     */
+    public $image = '';
+
+}
