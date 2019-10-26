e87013fce5b6e5e5d28b3f2e1228986fb34dd818
Mixno <mixno09@mail.ru>
2019-10-26 16:27:10 +0300

    Created controllers for the catalog with goods and a controller for the product

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index()
+    {
+        return $this->render('product/index.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    /**
+     * @Route("/product/create", name="product.create")
+     */
+    public function create()
+    {
+        return $this->render('product/form.html.twig');
+    }
+}
