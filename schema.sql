294ae1564770de1c5b720a48b7df1eba53a3858e
Mixno <mixno09@mail.ru>
2019-12-18 20:39:44 +0300

    Добавлена таблица для отзывов. Логика записи с загрузки отзывов с продуктом.

--- schema.sql
+++ schema.sql
@@ -1,23 +1,36 @@
+DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS products;
 
-CREATE TABLE users (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    email VARCHAR(320) NOT NULL,
-    admin BOOLEAN NOT NULL,
-    roles JSON NOT NULL,
-    password CHAR(60) NOT NULL,
+CREATE TABLE users
+(
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    email    VARCHAR(320) NOT NULL,
+    admin    BOOLEAN      NOT NULL,
+    roles    JSON         NOT NULL,
+    password CHAR(60)     NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT UNIQUE (email)
 ) ENGINE InnoDB;
 
-
-CREATE TABLE products (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    title VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    image TEXT NOT NULL,
+CREATE TABLE products
+(
+    id          INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title       VARCHAR(255) NOT NULL,
+    description TEXT         NOT NULL,
+    image       TEXT         NOT NULL,
     PRIMARY KEY (id)
+) ENGINE InnoDB;
+
+CREATE TABLE reviews
+(
+    id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    product_id INT UNSIGNED NOT NULL,
+    user_id    INT UNSIGNED NOT NULL,
+    `text`     TEXT         NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
 ) ENGINE InnoDB;
 
 
--- src/Repository/PDOProductRepository.php
+++ src/Repository/PDOProductRepository.php
@@ -5,15 +5,19 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Entity\Product;
+use App\Entity\Review;
+use App\Entity\User;
 use Knp\Bundle\PaginatorBundle\Definition\PaginatorAwareInterface;
 use Knp\Component\Pager\Event\Subscriber\Paginate\Callback\CallbackPagination;
 use Knp\Component\Pager\Pagination\PaginationInterface;
 use Knp\Component\Pager\Paginator;
 use PDO;
+use ReflectionClass;
 
 class PDOProductRepository implements ProductRepositoryInterface, PaginatorAwareInterface
 {
-    public const TABLE = 'products';
+    public const PRODUCT_TABLE = 'products';
+    public const REVIEW_TABLE = 'reviews';
 
     /**
      * @var \PDO
@@ -40,11 +44,12 @@ class PDOProductRepository implements ProductRepositoryInterface, PaginatorAware
         } else {
             $this->update($product);
         }
+        $this->persistReviews($product);
     }
 
     private function insert(Product $product): void
     {
-        $sql = 'INSERT INTO ' . self::TABLE . ' (title, description, image) VALUES (:title, :description, :image)';
+        $sql = 'INSERT INTO ' . self::PRODUCT_TABLE . ' (title, description, image) VALUES (:title, :description, :image)';
         $statement = $this->pdo->prepare($sql);
         $statement->bindValue(':title', $product->title, PDO::PARAM_STR);
         $statement->bindValue(':description', $product->description, PDO::PARAM_STR);
@@ -57,7 +62,7 @@ class PDOProductRepository implements ProductRepositoryInterface, PaginatorAware
 
     private function update(Product $product): void
     {
-        $sql = 'UPDATE ' . self::TABLE . ' SET title = :title, description = :description, image = :image WHERE id = :id';
+        $sql = 'UPDATE ' . self::PRODUCT_TABLE . ' SET title = :title, description = :description, image = :image WHERE id = :id';
         $statement = $this->pdo->prepare($sql);
         $statement->bindValue(':id', $product->id, PDO::PARAM_INT);
         $statement->bindValue(':title', $product->title, PDO::PARAM_STR);
@@ -68,29 +73,31 @@ class PDOProductRepository implements ProductRepositoryInterface, PaginatorAware
 
     public function find(int $id): ?Product
     {
-        $sql = 'SELECT * FROM ' . self::TABLE . ' WHERE id = :id';
+        $sql = 'SELECT * FROM ' . self::PRODUCT_TABLE . ' WHERE id = :id';
         $statement = $this->pdo->prepare($sql);
         $statement->bindValue(':id', $id, PDO::PARAM_INT);
         $statement->execute();
         $product = $statement->fetchObject(Product::class);
         if (! $product instanceof Product) {
-            $product = null;
+            return null;
         }
+        $this->loadReviews($product);
         return $product;
     }
 
     public function paginate(int $page = 1, int $limit = 10, array $options = []): PaginationInterface
     {
         $count = function () {
-            $sql = 'SELECT count(*) FROM ' . self::TABLE;
+            $sql = 'SELECT count(*) FROM ' . self::PRODUCT_TABLE;
             $statement = $this->pdo->query($sql);
             $count = $statement->fetch(PDO::FETCH_COLUMN);
             return $count;
         };
         $items = function ($offset, $limit) {
-            $sql = 'SELECT * FROM ' . self::TABLE . ' ORDER BY id ASC LIMIT ' . $limit . ' OFFSET ' . $offset;
+            $sql = 'SELECT * FROM ' . self::PRODUCT_TABLE . ' ORDER BY id ASC LIMIT ' . $limit . ' OFFSET ' . $offset;
             $statement = $this->pdo->query($sql);
             $items = $statement->fetchAll(PDO::FETCH_CLASS, Product::class);
+            $this->loadReviews($items);
             return $items;
         };
         $target = new CallbackPagination($count, $items);
@@ -100,7 +107,7 @@ class PDOProductRepository implements ProductRepositoryInterface, PaginatorAware
 
     public function delete(int $id): void
     {
-        $sql = 'DELETE FROM ' . self::TABLE . ' WHERE id = :id';
+        $sql = 'DELETE FROM ' . self::PRODUCT_TABLE . ' WHERE id = :id';
         $statement = $this->pdo->prepare($sql);
         $statement->bindValue(':id', $id, PDO::PARAM_INT);
         $statement->execute();
@@ -111,9 +118,10 @@ class PDOProductRepository implements ProductRepositoryInterface, PaginatorAware
      */
     public function newest(int $count): array
     {
-        $sql = 'SELECT * FROM ' . self::TABLE . ' ORDER BY id DESC LIMIT ' . $count;
+        $sql = 'SELECT * FROM ' . self::PRODUCT_TABLE . ' ORDER BY id DESC LIMIT ' . $count;
         $statement = $this->pdo->query($sql);
         $newest = $statement->fetchAll(PDO::FETCH_CLASS, Product::class);
+        $this->loadReviews($newest);
         return $newest;
     }
 
@@ -123,5 +131,99 @@ class PDOProductRepository implements ProductRepositoryInterface, PaginatorAware
     public function setPaginator(Paginator $paginator)
     {
         $this->paginator = $paginator;
+    }
+
+    private function persistReviews(Product $product): void
+    {
+        $reflectionClass = new ReflectionClass(Product::class);
+        $reflectionProperty = $reflectionClass->getProperty('reviews');
+        $reflectionProperty->setAccessible(true);
+        /** @var \App\Entity\Review[] $reviews */
+        $reviews = $reflectionProperty->getValue($product);
+
+        foreach ($reviews as $review) {
+            if ($review->id === 0) {
+                $this->insertReview($review, $product->id);
+            } else {
+                $this->updateReview($review);
+            }
+        }
+        $this->deleteReviews($reviews, $product->id);
+    }
+
+    private function insertReview(Review $review, int $productId): void
+    {
+        $sql = 'INSERT INTO ' . self::REVIEW_TABLE . ' (product_id, user_id, `text`) VALUES (:product_id, :user_id, :text)';
+        $statement = $this->pdo->prepare($sql);
+        $statement->bindValue(':product_id', $productId, PDO::PARAM_INT);
+        $statement->bindValue(':user_id', $review->author->id, PDO::PARAM_INT);
+        $statement->bindValue(':text', $review->text, PDO::PARAM_STR);
+        $statement->execute();
+
+        $id = $this->pdo->lastInsertId();
+        $review->id = $id;
+    }
+
+    private function updateReview(Review $review): void
+    {
+
+    }
+
+    private function deleteReviews(array $reviews, int $productId): void
+    {
+
+    }
+
+    private function loadReviews(Product ...$products): void
+    {
+        if (count($products) === 0) {
+            return;
+        }
+
+        $productId = array_column($products, 'id');
+        $sql =
+            'SELECT r.id, r.product_id, r.text, u.id AS u_id, u.email AS u_email, u.admin AS u_admin, u.roles AS u_roles, u.password AS u_password ' .
+            'FROM ' . self::REVIEW_TABLE . ' AS r ' .
+            'INNER JOIN ' . PDOUserRepository::TABLE . ' AS u ON r.user_id = u.id ' .
+            'WHERE r.product_id IN (' . implode(',', $productId) . ')';
+        $statement = $this->pdo->query($sql);
+        $data = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        $productReviewsMap = [];
+        foreach ($data as $value) {
+            $user = $this->hydrateUser($value);
+            $review = new Review($user, $value['text']);
+            $review->id = $value['id'];
+            $productId = $value['product_id'];
+            $productReviewsMap[$productId][] = $review;
+        }
+        foreach ($products as $product) {
+            $productId = $product->id;
+            $reviews = [];
+            if (array_key_exists($productId, $productReviewsMap)) {
+                $reviews = $productReviewsMap[$productId];
+            }
+            $reflectionClass = new ReflectionClass(Product::class);
+            $reflectionProperty = $reflectionClass->getProperty('reviews');
+            $reflectionProperty->setAccessible(true);
+            $reflectionProperty->setValue($product, $reviews);
+        }
+    }
+
+    private function hydrateUser(array $data): User
+    {
+        $user = new User();
+        $user->id = $data['u_id'];
+        $user->email = $data['u_email'];
+        $user->admin = ($data['u_admin'] === 1);
+
+        $reflectionClass = new ReflectionClass(User::class);
+        $reflectionProperty = $reflectionClass->getProperty('password');
+        $reflectionProperty->setAccessible(true);
+        $reflectionProperty->setValue($user, $data['u_password']);
+
+        $roles = json_decode($data['u_roles']);
+        $user->setRoles($roles);
+        return $user;
     }
 }
