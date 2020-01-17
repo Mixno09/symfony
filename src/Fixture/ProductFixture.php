fc55886e2152c4ca46d8bfa328e05e13e903ac44
Николай <kail_@mail.ru>
2020-01-17 20:18:53 +0300

    Исправлен баг с генерацией картинок в ProductFixture

--- src/Fixture/ProductFixture.php
+++ src/Fixture/ProductFixture.php
@@ -3,11 +3,12 @@
 
 namespace App\Fixture;
 
-
 use App\Entity\Product;
 use App\Repository\ProductRepositoryInterface;
 use App\Service\FileManager;
 use Faker\Factory;
+use Faker\Generator;
+use RuntimeException;
 use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -41,9 +42,8 @@ class ProductFixture extends AbstractFixture
             $product = new Product();
             $product->title = $faker->sentence(3);
             $product->description = $faker->realText(300);
-            $image = $faker->image(null, 400, 400);
-            $uploadFile = new UploadedFile($image, basename($image), null, null, true);
-            $image = $this->fileManager->upload($uploadFile);
+            $image = $this->generateImage($faker);
+            $image = $this->fileManager->upload($image);
             $product->image = $image;
             $this->repository->save($product);
             $count--;
@@ -53,5 +53,40 @@ class ProductFixture extends AbstractFixture
     public function getName(): string
     {
         return 'product';
+    }
+
+    public function generateImage(Generator $faker): UploadedFile
+    {
+        $image = imagecreate(400, 400);
+        if (! is_resource($image)) {
+            throw new RuntimeException('Image is not resource');
+        }
+
+        $color = imagecolorallocate($image, ...$faker->rgbColorAsArray);
+        if (! is_int($color)) {
+            throw new RuntimeException('Color is not int');
+        }
+
+        $isFillImage = imagefill($image, 0,0, $color);
+        if (! $isFillImage) {
+            throw new RuntimeException('Image is not fill');
+        }
+
+        $file = tempnam(sys_get_temp_dir(), '');
+        if (! is_string($file)) {
+            throw new RuntimeException('File is not created');
+        }
+
+        $isSaveImage = imagejpeg($image, $file);
+        if (! $isSaveImage) {
+            throw new RuntimeException('Failed to save image');
+        }
+
+        $isDestroyImage = imagedestroy($image);
+        if (! $isDestroyImage) {
+            throw new RuntimeException('Failed to destroy image');
+        }
+
+        return new UploadedFile($file, basename($file), null, null, true);
     }
 }
