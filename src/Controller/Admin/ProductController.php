e22c3045448ac458600d63a4bb283111319d2003
Mixno <mixno09@mail.ru>
2020-04-03 18:54:24 +0300

    Добавлен пакет knplabs/knp-paginator-bundle, страница продуктов в админке и пагинация на неё.

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -11,6 +11,7 @@ use App\UseCase\CreateProduct\Handler as CreateHandler;
 use App\UseCase\UpdateProduct\Command as UpdateCommand;
 use App\UseCase\UpdateProduct\Handler as UpdateHandler;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,47 +20,35 @@ use Symfony\Component\Routing\Annotation\Route;
 final class ProductController extends AbstractController
 {
     /**
-     * @var \App\UseCase\CreateProduct\Handler
-     */
-    private $createHandler;
-    /**
-     * @var UpdateHandler
-     */
-    private $updateHandler;
-    /**
-     * @var \Doctrine\ORM\EntityManagerInterface
-     */
-    private $entityManager;
-
-    /**
-     * ProductController constructor.
+     * @Route("/admin/product", name="product_index", methods={"GET"})
      * @param \Doctrine\ORM\EntityManagerInterface $entityManager
-     * @param \App\UseCase\CreateProduct\Handler $createHandler
-     * @param \App\UseCase\UpdateProduct\Handler $updateHandler
+     * @param \Knp\Component\Pager\PaginatorInterface $paginator
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function __construct(
-        EntityManagerInterface $entityManager,
-        CreateHandler $createHandler,
-        UpdateHandler $updateHandler
-    ) {
-        $this->createHandler = $createHandler;
-        $this->entityManager = $entityManager;
-        $this->updateHandler = $updateHandler;
+    public function index(EntityManagerInterface $entityManager, PaginatorInterface $paginator): Response
+    {
+        $query = $entityManager->createQuery('SELECT p FROM ' . Product::class . ' p');
+        $pagination = $paginator->paginate($query);
+
+        return $this->render('admin/product/index.html.twig', [
+            'pagination' => $pagination,
+        ]);
     }
 
     /**
      * @Route("/admin/product/create", name="product_create", methods={"GET", "POST"})
      * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \App\UseCase\CreateProduct\Handler $handler
      * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Throwable
      */
-    public function create(Request $request): Response
+    public function create(Request $request, CreateHandler $handler): Response
     {
         $command = new CreateCommand();
         $form = $this->createForm(ProductType::class, $command);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $id = $this->createHandler->execute($command);
+            $id = $handler->execute($command);
             return $this->redirectToRoute('product_update', ['id' => $id]);
         }
         return $this->render('admin/product/create.html.twig', [
@@ -71,12 +60,14 @@ final class ProductController extends AbstractController
      * @Route("/admin/product/{id}/update", name="product_update", methods={"GET", "POST"}, requirements={"id"="\d+"})
      * @param int $id
      * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \App\UseCase\UpdateProduct\Handler $handler
      * @return \Symfony\Component\HttpFoundation\Response
      * @throws \Throwable
      */
-    public function update(int $id, Request $request): Response
+    public function update(int $id, Request $request, UpdateHandler $handler): Response
     {
-        $product = $this->entityManager->find(Product::class, $id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $product = $entityManager->find(Product::class, $id);
         if (! $product instanceof Product) {
             throw $this->createNotFoundException("Продукта с ID={$id} не существует");
         }
@@ -86,7 +77,7 @@ final class ProductController extends AbstractController
         $form = $this->createForm(ProductType::class, $command);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->updateHandler->execute($command);
+            $handler->execute($command);
             return $this->redirectToRoute('product_update', ['id' => $id]);
         }
         return $this->render('admin/product/update.html.twig', [
