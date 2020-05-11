f1b8a1480780126649cdcdacc5dde76c44247640
Николай <kail_@mail.ru>
2020-05-11 13:48:39 +0300

    Added TypeScript

---
+++ assets/js/common/APIUtils.ts
@@ -0,0 +1,16 @@
+export function makeFormData(data: object): FormData {
+    const formData = new FormData();
+    for (let name in data) {
+        if (! data.hasOwnProperty(name)) {
+            continue;
+        }
+
+        const value = data[name];
+        if (value instanceof File) {
+            formData.append(name, value, value.name);
+        } else {
+            formData.append(name, value);
+        }
+    }
+    return formData;
+}
